1f1603ee33ef6418798d3d60b802b993955f52a6
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-12-08 23:20:48 +0100

    more source

--- src/dplyr.cpp
+++ src/dplyr.cpp
@@ -4,6 +4,17 @@ using namespace Rcpp ;
 using namespace dplyr ;
 
 // [[Rcpp::export]]
+SEXP shallow_copy(const List& data){
+    int n = data.size() ;
+    List out(n) ;
+    for( int i=0; i<n; i++) {
+      out[i] = shared_SEXP(data[i]) ;
+    }
+    copy_attributes(out, data) ;
+    return out ;
+}
+
+// [[Rcpp::export]]
 DataFrame as_regular_df(DataFrame df){
   DataFrame copy( shallow_copy(df));
   SET_ATTRIB(copy, strip_group_attributes(df)) ;
