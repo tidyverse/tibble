0a5657b60ecd34854d3272dbdd318220937261d2
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-12-08 23:53:00 +0100

    merge tests

--- tests/testthat/test-data_frame.R
+++ tests/testthat/test-data_frame.R
@@ -24,6 +24,27 @@ test_that("null isn't a valid column", {
   expect_error(as_data_frame(list(a = NULL)), "only contain 1d atomic vectors and lists")
 })
 
+test_that("length 1 vectors are recycled", {
+  expect_equal(nrow(data_frame(x = 1:10)), 10)
+  expect_equal(nrow(data_frame(x = 1:10, y = 1)), 10)
+  expect_error(
+    nrow(data_frame(x = 1:10, y = 1:2)),
+    "Variables must be length 1 or 10"
+  )
+})
+
+test_that("missing names are imputed from call", {
+  x <- 1:10
+  df <- data_frame(x, y = x)
+  expect_equal(names(df), c("x", "y"))
+})
+
+test_that("empty input makes 0 x 0 tbl_df", {
+  zero <- data_frame()
+  expect_is(zero, "tbl_df")
+  expect_equal(dim(zero), c(0L, 0L))
+})
+
 # as_data_frame -----------------------------------------------------------
 
 test_that("columns must be same length", {
@@ -54,4 +75,58 @@ test_that("error on NA column names (#1101)", {
   df <- data.frame( x = 1:10, y = 1:10 )
   names(df)[1] <- NA
   expect_error( as_data_frame(df), "All columns must be named" )
+})
+
+test_that("add_rownames keeps the tbl classes (#882)", {
+  res <- add_rownames( mtcars, "Make&Model" )
+  expect_equal( class(res), c("tbl_df","tbl", "data.frame"))
+})
+
+# Validation --------------------------------------------------------------
+
+test_that("2d object isn't a valid column", {
+  expect_error(
+    check_data_frame(list(x = mtcars)),
+    "Each variable must be a 1d atomic vector"
+  )
+})
+
+test_that("POSIXlt isn't a valid column", {
+  expect_error(
+    check_data_frame(list(x = as.POSIXlt(Sys.time()))),
+    "Date/times must be stored as POSIXct"
+  )
+})
+
+test_that("NULL isn't a valid column", {
+  expect_error(
+    check_data_frame(list(a = NULL)),
+    "Each variable must be a 1d atomic vector"
+  )
+})
+
+test_that("columns must be named (#1101)", {
+  l <- list(1:10, 1:10)
+
+  expect_error(
+    check_data_frame(l),
+    "Each variable must be named"
+  )
+
+  expect_error(
+    check_data_frame(setNames(l, c("x", ""))),
+    "Each variable must be named"
+  )
+
+  expect_error(
+    check_data_frame(setNames(l, c("x", NA))),
+    "Each variable must be named"
+  )
+})
+
+test_that("names must be unique (#820)", {
+  expect_error(
+    check_data_frame(list(x = 1, x = 2)),
+    "Each variable must have a unique name"
+  )
 })
