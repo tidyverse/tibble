2c0f36c661499e13f4f8e331c3c59109c35b6691
Bill Evans <BillEvans@ActiveDecisionSupport.com>
2016-01-11 10:30:35 -0800

    repair_names: changed indent from 4 to 2

--- R/repair-column-names.R
+++ R/repair-column-names.R
@@ -9,26 +9,25 @@
 #' @return data.frame with possibly-repaired column names
 #' @export
 repair_names <- function(x, prefix = "V", sep = "") {
+  col_names <- colnames(x)
+  col_names <-
+    if (is.null(col_names)) {
+      col_names <- rep('', ncol(x))
+    } else {
+      ifelse(is.na(col_names), '', col_names)
+    }
+  sub_names <- col_names[ col_names != '' ]
+  sub_names <- make.unique(sub_names, sep = sep)
+  col_names[ col_names != '' ] <- sub_names
 
-    col_names <- colnames(x)
-    col_names <-
-        if (is.null(col_names)) {
-            col_names <- rep('', ncol(x))
-        } else {
-            ifelse(is.na(col_names), '', col_names)
-        }
-    sub_names <- col_names[ col_names != '' ]
-    sub_names <- make.unique(sub_names, sep = sep)
-    col_names[ col_names != '' ] <- sub_names
+  nmissing <- sum(col_names == '')
+  new_names <- paste(prefix, 1:ceiling(1.5 * nmissing), sep = sep)
+  new_names <- head(setdiff(new_names,
+                            intersect(col_names, new_names)),
+                    n = sum(col_names == ''))
 
-    nmissing <- sum(col_names == '')
-    new_names <- paste(prefix, 1:ceiling(1.5 * nmissing), sep = sep)
-    new_names <- head(setdiff(new_names,
-                              intersect(col_names, new_names)),
-                      n = sum(col_names == ''))
+  col_names[ col_names == '' ] <- new_names
 
-    col_names[ col_names == '' ] <- new_names
-
-    colnames(x) <- col_names
-    x
+  colnames(x) <- col_names
+  x
 }
