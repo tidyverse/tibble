7db7b52c5091798bfee89237c1337bc6699dfcd3
Bill Evans <BillEvans@ActiveDecisionSupport.com>
2016-03-03 20:33:47 -0800

    whitespace in test-repair_names

--- tests/testthat/test-repair_names.R
+++ tests/testthat/test-repair_names.R
@@ -1,68 +1,68 @@
 context("repair_names")
 
 test_that("repair missing column names", {
-    dat <- data.frame(a = 1, b = 2, c = 3)
-    colnames(dat)[2] <- NA
+  dat <- data.frame(a = 1, b = 2, c = 3)
+  colnames(dat)[2] <- NA
 
-    # ensure we start with a "bad" state
-    expect_true(any(sapply(colnames(dat), is.na)))
+  # ensure we start with a "bad" state
+  expect_true(any(sapply(colnames(dat), is.na)))
 
-    fixed_dat <- repair_names(dat)
-    fixed_names <- colnames(fixed_dat)
-    # no empty names
-    expect_false(any(sapply(fixed_names, is.null)))
-    # no repeats
-    expect_false(any(table(fixed_names) > 1))
+  fixed_dat <- repair_names(dat)
+  fixed_names <- colnames(fixed_dat)
+  # no empty names
+  expect_false(any(sapply(fixed_names, is.null)))
+  # no repeats
+  expect_false(any(table(fixed_names) > 1))
 
-    # ensure all valid column names are retained
-    expect_equal(length(setdiff(Filter(function(a) ! (is.na(a) | a == ''),
-                                       colnames(dat)),
-                                fixed_names)), 0)
+  # ensure all valid column names are retained
+  expect_equal(length(setdiff(Filter(function(a) ! (is.na(a) | a == ''),
+                                     colnames(dat)),
+                              fixed_names)), 0)
 })
 
 test_that("repair various name problems", {
-    combos <- list(Null = NULL,
-                   Empty = c('', '', ''),
-                   Spaces = c('a', 'b', ' '),
-                   EmptyWithNA = c('', NA, NA),
-                   Dup1 = c('a', 'a', 'b'),
-                   Evil1 = c('a', 'a', 'a1'),
-                   OneNA = c('a', 'b', NA),
-                   Missing2 = c('', '', 'b'),
-                   Vnames1 = c('V1', '', ''),
-                   Vnames2 = c('V2', '', ''),
-                   Vnames3 = c('V1', '', 'a'),
-                   VnamesDup1 = c('V1', 'V1', 'c'),
-                   VnamesDup2 = c('V1', 'V1', '')
-                   )
-    for (combo_name in names(combos)) {
-        dat <- data.frame(a = 1, b = 2, c = 3)
-        colnames(dat) <- combos[[ combo_name ]]
+  combos <- list(Null = NULL,
+                 Empty = c('', '', ''),
+                 Spaces = c('a', 'b', ' '),
+                 EmptyWithNA = c('', NA, NA),
+                 Dup1 = c('a', 'a', 'b'),
+                 Evil1 = c('a', 'a', 'a1'),
+                 OneNA = c('a', 'b', NA),
+                 Missing2 = c('', '', 'b'),
+                 Vnames1 = c('V1', '', ''),
+                 Vnames2 = c('V2', '', ''),
+                 Vnames3 = c('V1', '', 'a'),
+                 VnamesDup1 = c('V1', 'V1', 'c'),
+                 VnamesDup2 = c('V1', 'V1', '')
+                 )
+  for (combo_name in names(combos)) {
+    dat <- data.frame(a = 1, b = 2, c = 3)
+    colnames(dat) <- combos[[ combo_name ]]
 
-        # ensure we start with a "bad" state
-        old_names <- colnames(dat)
-        expect_true(is.null(old_names) ||
-                        any(table(old_names) > 1) ||
-                        any(old_names == '' | is.na(old_names)) ||
-                        any(grepl('^ +', old_names)),
-                    info = combo_name)
+    # ensure we start with a "bad" state
+    old_names <- colnames(dat)
+    expect_true(is.null(old_names) ||
+                  any(table(old_names) > 1) ||
+                  any(old_names == '' | is.na(old_names)) ||
+                  any(grepl('^ +', old_names)),
+                info = combo_name)
 
-        fixed_dat <- repair_names(dat)
-        fixed_names <- colnames(fixed_dat)
+    fixed_dat <- repair_names(dat)
+    fixed_names <- colnames(fixed_dat)
 
-        # no repeats
-        expect_false(any(table(fixed_names) > 1), info = combo_name)
+    # no repeats
+    expect_false(any(table(fixed_names) > 1), info = combo_name)
 
-        # ensure all valid column names are retained
-        if (! is.null(old_names)) {
-            valid <- ! is.na(old_names) & old_names != '' &
-                ! duplicated(old_names) & ! grepl('^ +', old_names)
-            expect_true(all(fixed_names[valid] == old_names[valid]))
-        }
+    # ensure all valid column names are retained
+    if (! is.null(old_names)) {
+      valid <- ! is.na(old_names) & old_names != '' &
+        ! duplicated(old_names) & ! grepl('^ +', old_names)
+      expect_true(all(fixed_names[valid] == old_names[valid]))
     }
+  }
 })
 
 test_that("check pathological cases", {
-    expect_null(repair_names(data.frame()))
-    expect_null(repair_names(data.frame(row.names = 1:10)))
+  expect_null(repair_names(data.frame()))
+  expect_null(repair_names(data.frame(row.names = 1:10)))
 })
