eb40450a117c9ceabba4e3ccf81990c18e9d7a3a
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-03-09 11:46:59 +0100

    document optons in package doc

--- R/zzz.r
+++ R/zzz.r
@@ -1,10 +1,23 @@
+#' @name tibble-package
+#' @section Package options:
+#' \describe{
+(op.tibble <- lst(
+  #' \item{\code{tibble.print_max}}{Row number threshold: Maximum number of rows
+  #'   printed. Set to \code{Inf} to always print all rows.  Default: 20.}
+  tibble.print_max = 20L,
+
+  #' \item{\code{tibble.print_min}}{Number of rows printed if row number
+  #'   threshold is exceeded. Default: 10.}
+  tibble.print_min = 10L,
+
+  #' \item{\code{tibble.width}}{Output width. Default: \code{NULL} (use
+  #'   \code{width} option).}
+  tibble.width = NULL
+#' }
+))
+
 .onLoad <- function(libname, pkgname) {
   op <- options()
-  op.tibble <- list(
-    tibble.width = NULL,
-    tibble.print_min = 10L,
-    tibble.print_max = 20L
-  )
   toset <- !(names(op.tibble) %in% names(op))
   if(any(toset)) options(op.tibble[toset])
 
