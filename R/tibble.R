af67d9fa9d731e12c09b7fdff9edb66b30954f9b
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-03-09 12:13:14 +0100

    getting started

--- R/tibble.R
+++ R/tibble.R
@@ -3,4 +3,9 @@
 #' @import assertthat
 #' @importFrom utils head tail
 #' @aliases NULL
+#' @section Getting started:
+#' See \code{\link{tbl_df}} for an introduction,
+#' \code{\link{data_frame}} and \code{\link{frame_data}} for construction,
+#' \code{\link{as_data_frame}} for coercion,
+#' and \code{\link{print.tbl_df}} and \code{\link{glimpse}} for display.
 "_PACKAGE"
