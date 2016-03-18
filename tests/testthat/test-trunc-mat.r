1efc49d00fd895acf0b50ddb19e5538499103ba5
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-03-18 21:40:07 +0100

    subdir

--- tests/testthat/test-trunc-mat.r
+++ tests/testthat/test-trunc-mat.r
@@ -3,37 +3,36 @@ context("Truncated matrix")
 test_that("trunc_mat output matches known output", {
   expect_output_identical(
     print(tbl_df(mtcars), n = 8L, width = 30L),
-    "mtcars-8-30.txt")
+    "trunc_mat/mtcars-8-30.txt")
 
   expect_output_identical(
     print(tbl_df(iris), n = 5L, width = 30L),
-    "iris-5-30.txt")
+    "trunc_mat/iris-5-30.txt")
 
   expect_output_identical(
     print(tbl_df(iris), n = 3L, width = 5L),
-    "iris-3-5.txt")
+    "trunc_mat/iris-3-5.txt")
 
   expect_output_identical(
     print(df_all, n = NULL, width = 30L),
-    "all--30.txt")
+    "trunc_mat/all--30.txt")
 
   expect_output_identical(
     print(data_frame(a = character(), b = logical()), width = 30L),
-    "zero_rows--30.txt")
+    "trunc_mat/zero_rows--30.txt")
 
   expect_output_identical(
     print(tbl_df(iris)[character()], n = 5L, width = 30L),
-    "zero_cols-5-30.txt")
+    "trunc_mat/zero_cols-5-30.txt")
 
   expect_output_identical(
     print(trunc_mat(df_all, n = 1L, n_extra = 2L, width = 30L)),
-    "all-1-30-2.txt")
+    "trunc_mat/all-1-30-2.txt")
 
   knit <- knitr::knit_print(trunc_mat(df_all, width = 60L))
   expect_output_identical(
     print(knit),
-    "knit.txt")
+    "trunc_mat/knit.txt")
   expect_is(knit, "knit_asis")
   expect_true(attr(knit, "knit_cacheable"))
 })
-
