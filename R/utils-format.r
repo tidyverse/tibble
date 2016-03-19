c42220aaf5327a837327344c3d78e1716f12f2b2
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-03-19 21:30:51 +0100

    get rid of dim_desc()

--- R/utils-format.r
+++ R/utils-format.r
@@ -12,7 +12,6 @@
 #' @seealso \link{tibble-package}
 #' @keywords internal
 #' @examples
-#' dim_desc(mtcars)
 #' trunc_mat(mtcars)
 #'
 #' print(tbl_df(mtcars))
@@ -22,16 +21,6 @@
 #'
 #' @name formatting
 NULL
-
-#' @export
-#' @rdname formatting
-dim_desc <- function(x) {
-  d <- dim(x)
-  d2 <- big_mark(d)
-  d2[is.na(d)] <- "??"
-
-  paste0("[", paste0(d2, collapse = " x "), "]")
-}
 
 #' @export
 #' @rdname formatting
