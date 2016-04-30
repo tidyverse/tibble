53d0a64a110531aeac2c33899020a802e755bced
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-04-30 22:07:24 +0200

    rename

--- tests/testthat/helper-output.R
+++ tests/testthat/helper-output.R
@@ -1,5 +1,5 @@
 output_file <- function(filename) file.path("output", filename)
 
-expect_output_identical <- function(x, filename) {
+expect_output_file_rel <- function(x, filename) {
   expect_output_file(x, output_file(filename), update = TRUE)
 }
--- tests/testthat/test-glimpse.R
+++ tests/testthat/test-glimpse.R
@@ -1,30 +1,30 @@
 context("Glimpse")
 
 test_that("glimpse output matches known output", {
-  expect_output_identical(
+  expect_output_file_rel(
     glimpse(as_data_frame(mtcars), width = 70L),
     "glimpse/mtcars-70.txt")
 
-  expect_output_identical(
+  expect_output_file_rel(
     glimpse(as_data_frame(iris), width = 70L),
     "glimpse/iris-70.txt")
 
-  expect_output_identical(
+  expect_output_file_rel(
     glimpse(as_data_frame(iris[integer()]), width = 70L),
     "glimpse/iris-empty-70.txt")
 
-  expect_output_identical(
+  expect_output_file_rel(
     glimpse(as_data_frame(df_all), width = 70L),
     "glimpse/all-70.txt")
 
   withr::with_options(
     list(tibble.width = 50),
-    expect_output_identical(
+    expect_output_file_rel(
       glimpse(as_data_frame(df_all)),
       "glimpse/all-50.txt")
   )
 
-  expect_output_identical(
+  expect_output_file_rel(
     glimpse(5),
     "glimpse/5.txt")
 })
