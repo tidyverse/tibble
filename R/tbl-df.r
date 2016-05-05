ef2be13d665563428185adfb7b9aeaad8695df39
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-05-06 00:13:32 +0200

    Revert "Discourage double-index subsetting"
    This reverts commit 1f2db4541bf5be7b41f1f5272a65c4a62ccd848d.

--- R/tbl-df.r
+++ R/tbl-df.r
@@ -28,11 +28,8 @@ print.tbl_df <- function(x, ..., n = NULL, width = NULL) {
 `[[.tbl_df` <- function(x, i, j, ..., exact = TRUE) {
   if (missing(j))
     colname <- i
-  else {
+  else
     colname <- j
-    warning("Passing two subscripts via x[[i, j]] is deprecated, use x[[j]][[i]] instead.",
-            call. = FALSE)
-  }
   if (is.character(colname) && length(colname) == 1L && !(colname %in% names(x))) {
     stop("Unknown column '", colname, "'", call. = FALSE)
   }
