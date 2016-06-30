2c9b4f59f0ac6ac83ecf26306fe11d56c81bcb6d
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-07-01 01:18:58 +0200

    rename in tests

--- tests/testthat/test-glimpse.R
+++ tests/testthat/test-glimpse.R
@@ -31,32 +31,32 @@ test_that("format_v for list", {
 
 test_that("glimpse output matches known output", {
   expect_output_file_rel(
-    glimpse(as_data_frame(mtcars), width = 70L),
+    glimpse(as_tibble(mtcars), width = 70L),
     "glimpse/mtcars-70.txt")
 
   expect_output_file_rel(
-    glimpse(as_data_frame(iris), width = 70L),
+    glimpse(as_tibble(iris), width = 70L),
     "glimpse/iris-70.txt")
 
   expect_output_file_rel(
-    glimpse(as_data_frame(iris[integer()]), width = 70L),
+    glimpse(as_tibble(iris[integer()]), width = 70L),
     "glimpse/iris-empty-70.txt")
 
   expect_output_file_rel(
-    glimpse(as_data_frame(df_all), width = 70L),
+    glimpse(as_tibble(df_all), width = 70L),
     "glimpse/all-70.txt")
 
   withr::with_options(
     list(tibble.width = 50),
     expect_output_file_rel(
-      glimpse(as_data_frame(df_all)),
+      glimpse(as_tibble(df_all)),
       "glimpse/all-50.txt")
   )
 
   withr::with_options(
     list(tibble.width = 35),
     expect_output_file_rel(
-      glimpse(as_data_frame(df_all)),
+      glimpse(as_tibble(df_all)),
       "glimpse/all-35.txt")
   )
 
