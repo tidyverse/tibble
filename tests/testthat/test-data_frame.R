83af26cc2e9c742bafc9a20b9c020efb9a7bf6fc
Kirill Müller <krlmlr+r@mailbox.org>
2016-08-15 22:32:17 +0200

    update and split tests

--- tests/testthat/test-data_frame.R
+++ tests/testthat/test-data_frame.R
@@ -213,17 +213,22 @@ test_that("names must be unique (#820)", {
 
 test_that("can add new row", {
   df_all_new <- add_row(df_all, a = 4, b = 3L)
+  expect_identical(colnames(df_all_new), colnames(df_all))
   expect_identical(nrow(df_all_new), nrow(df_all) + 1L)
   expect_identical(df_all_new$a, c(df_all$a, 4))
   expect_identical(df_all_new$b, c(df_all$b, 3L))
   expect_identical(df_all_new$c, c(df_all$c, NA))
+})
 
+test_that("add_row() keeps class of object", {
   iris_new <- add_row(iris, Species = "unknown")
   expect_equal(class(iris), class(iris_new))
 
   iris_new <- add_row(as_tibble(iris), Species = "unknown")
   expect_equal(class(as_tibble(iris)), class(iris_new))
+})
 
+test_that("adds empty row if no arguments", {
   new_iris_row <- add_row(iris)[nrow(iris) + 1, , drop = TRUE]
   expect_true(all(is.na(new_iris_row)))
 })
