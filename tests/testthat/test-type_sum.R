bf8230cfdb7d791c745c02762139ff0b944da432
David Holstius <david.holstius@gmail.com>
2016-11-27 16:17:43 -0800

    Assign type_sum.SomeClass to .GlobalEnv (temporarily)
    Simply defining type_sum.SomeClass inside a test_that block doesn't
    seem to work.
    
    So, let's assign it to .GlobalEnv and then politely destroy it when
    we're done testing.

--- tests/testthat/test-type_sum.R
+++ tests/testthat/test-type_sum.R
@@ -4,9 +4,16 @@ SomeClass <- function (x) {
   structure(x, class = "SomeClass")
 }
 
-type_sum.SomeClass <- function (x, ...) {
-  "SC"
-}
+# Simply defining type_sum.SomeClass here
+# (or inside the following test_that block)
+# doesn't seem to work.
+#
+# So, let's assign it to .GlobalEnv and then
+# politely destroy it when we're done testing.
+#
+assign("type_sum.SomeClass",
+       function (x, ...) { "SC" },
+       .GlobalEnv)
 
 test_that("works with glimpse", {
 
@@ -14,4 +21,13 @@ test_that("works with glimpse", {
   expect_equal(type_sum(foo), "SC")
   expect_output(glimpse(tibble(foo)), "foo <SC>")
 
+})
+
+rm("type_sum.SomeClass",
+   envir = .GlobalEnv)
+
+test_that("teardown succeeded", {
+  expect_error(
+    get("type_sum.SomeClass"),
+    "object 'type_sum.SomeClass' not found")
 })
