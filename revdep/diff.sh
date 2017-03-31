ae4efcc1fe672d646db636d211d701807de0cff1
Kirill Müller <krlmlr+r@mailbox.org>
2017-03-31 11:39:48 +0000

    set flag from incoming check

--- revdep/diff.sh
+++ revdep/diff.sh
@@ -23,7 +23,7 @@ rm -rf revdep/install
 git add revdep
 git commit -m "revdep update for clean state" || true
 
-R -f revdep/check.R
+_R_CHECK_SUGGESTS_ONLY_=true R -f revdep/check.R
 #echo $old_tag >> revdep/README.md
 
 mv revdep revdep-$old_tag
@@ -49,7 +49,7 @@ git fetch --all
 git rebase
 git push origin HEAD
 
-R -f revdep/check.R
+_R_CHECK_SUGGESTS_ONLY_=true R -f revdep/check.R
 #echo $branch >> revdep/README.md
 
 cp revdep/README.md revdep/README-${branch}.md
