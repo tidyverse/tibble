589841f77eba37892a1bf712675aee7b0a29ecd1
Kirill Müller <krlmlr+r@mailbox.org>
2017-04-17 23:50:06 +0200

    backport changes

--- R/lst.R
+++ R/lst.R
@@ -4,38 +4,45 @@
 #' evaluates its arguments lazily and in order, and automatically adds names.
 #'
 #' @export
-#' @inheritParams tibble
-lst <- function(...) {
-  lst_(lazyeval::lazy_dots(...))
-}
+#' @examples
+#' lst(n = 5, x = runif(n))
+#'
+#' # You can splice-unquote a list of quotes and formulas
+#' lst(!!! list(n = ~5, y = quote(runif(n))))
 
 #' @export
-#' @rdname lst
-lst_ <- function(xs) {
-  n <- length(xs)
-  if (n == 0) return(list())
+#' @rdname tibble
+lst <- function(...) {
+  xs <- tidy_quotes(..., .named = 500L)
 
-  # If named not supplied, used deparsed expression
-  col_names <- names2(xs)
-  missing_names <- col_names == ""
-  if (any(missing_names)) {
-    deparse2 <- function(x) paste(deparse(x$expr, 500L), collapse = "")
-    defaults <- vapply(xs[missing_names], deparse2, character(1),
-      USE.NAMES = FALSE)
-    col_names[missing_names] <- defaults
+  n <- length(xs)
+  if (n == 0) {
+    return(list())
   }
 
   # Evaluate each column in turn
-  output <- vector("list", n)
+  col_names <- names2(xs)
+  output <- new_lst(n)
   names(output) <- character(n)
 
   for (i in seq_len(n)) {
-    res <- lazyeval::lazy_eval(xs[[i]], output)
-    if (!is.null(res)) {
+    res <- tidy_eval(xs[[i]], output)
+    if (!is_null(res)) {
       output[[i]] <-  res
     }
     names(output)[i] <- col_names[[i]]
   }
 
   output
+}
+
+discard_unnamed <- function(x) {
+  discard(x, names2(x) == "")
+}
+
+#' @export
+#' @rdname tibble
+lst_ <- function(xs) {
+  xs <- compat_lazy_dots(xs, caller_env())
+  lst(!!! xs)
 }
