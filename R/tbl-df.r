ecad80f3c804064db807e7c5453618a34137f843
Kirill Müller <krlmlr+r@mailbox.org>
2017-05-27 11:44:59 +0200

    Revert "explicit forwarding"
    This reverts commit 381d6f2a900a928b46aa753a0762141fb089314a.

--- R/tbl-df.r
+++ R/tbl-df.r
@@ -23,9 +23,7 @@ print.tbl <- function(x, ..., n = NULL, width = NULL, n_extra = NULL) {
 
 #' @rdname formatting
 #' @export
-print.tbl_df <- function(x, ..., n = NULL, width = NULL, n_extra = NULL) {
-  print.tbl(x, ..., n = n, width = width, n_extra = n_extra)
-}
+print.tbl_df <- print.tbl
 
 #' @export
 `[[.tbl_df` <- function(x, i, j, ..., exact = TRUE) {
