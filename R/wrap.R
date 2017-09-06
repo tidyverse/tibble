2981b49aca4d0d445b27fe63bb7c488efa2f2e24
Kirill Müller <krlmlr+r@mailbox.org>
2017-09-06 11:58:21 +0200

    implement simple variant of col_strwrap()
    1. split at space
    2. convert words to a sequence of dots corresponding to their display width
    3. word-wrap the sequence of dots
    4. split at dots
    5. paste spaces and dots together
    
    Double delimiters (newline or space) are treated specially by `strwrap()` but not by this implementation.
    
    CC @gaborcsardi

--- R/wrap.R
+++ R/wrap.R
@@ -2,16 +2,38 @@ NBSP <- "\U00A0"
 
 wrap <- function(..., indent = 0, prefix = "", width) {
   x <- paste0(..., collapse = "")
-  wrapped <- col_strwrap(x, indent, width - nchar_width(prefix))
+  wrapped <- strwrap2(x, width - nchar_width(prefix), indent)
   wrapped <- paste0(prefix, wrapped)
   wrapped <- gsub(NBSP, " ", wrapped)
 
   paste0(wrapped, collapse = "\n")
 }
 
-col_strwrap <- function(x, indent, width) {
-  strwrap(
-    x, indent = indent, exdent = indent + 2,
-    width = max(width, 0)
+strwrap2 <- function(x, width, indent) {
+  col_strwrap(x, width = max(width, 0), indent = indent, exdent = indent + 2)
+}
+
+col_strwrap <- function(x, width, indent, exdent) {
+  space_rx <- "[[:space:]]+"
+  words <- crayon::col_strsplit(x, space_rx, perl = TRUE)[[1L]]
+  words_bw <- strsplit(crayon::strip_style(x), space_rx, perl = TRUE)[[1L]]
+  stopifnot(length(words) == length(words_bw))
+
+  dots_list <- map(nchar_width(words_bw), rep, x = ".")
+  dots <- map_chr(dots_list, paste, collapse = "")
+
+  wrapped_dots <- strwrap(
+    paste(dots, collapse = " "),
+    width = width,
+    indent = indent,
+    exdent = exdent,
+    prefix = "",
+    simplify = TRUE,
+    initial = ""
   )
+  wrapped_dots_string <- paste(wrapped_dots, collapse = "\n")
+
+  space <- strsplit(wrapped_dots_string, "[.]+",)[[1L]]
+  wrapped_styled_string <- paste0(c("", words), c(space, ""), collapse = "")
+  strsplit(wrapped_styled_string, "\n", fixed = TRUE)[[1L]]
 }
