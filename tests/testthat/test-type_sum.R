b7770cb422a9dd76c36d9e7c34d260b01c9f190b
Kirill Müller <krlmlr+r@mailbox.org>
2017-11-03 08:15:56 +0100

    add override of [ to keep test unchanged

--- tests/testthat/test-type_sum.R
+++ tests/testthat/test-type_sum.R
@@ -3,5 +3,5 @@ context("type_sum")
 test_that("works with glimpse", {
   foo <- as_override_type_sum(2011:2013)
   expect_equal(type_sum(foo), "SC")
-  expect_output(glimpse(tibble(foo)), "foo <int>")
+  expect_output(glimpse(tibble(foo)), "foo <SC>")
 })
