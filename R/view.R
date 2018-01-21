8b0ddcf167f1eb89b6a37d6139a1e296afa3a5d3
Kirill Müller <krlmlr+r@mailbox.org>
2018-01-22 00:44:38 +0100

    document params

--- R/view.R
+++ R/view.R
@@ -3,6 +3,10 @@
 #' The default method of this generic calls [View()] (only in interactive
 #' sessions) and returns the input.
 #'
+#' @param x The object to view
+#' @param title The title to display, by default the unquoted expression
+#' @param ... Unused, for extensibility
+#'
 #' @export
 #' @examples
 #' \dontrun{
@@ -13,7 +17,7 @@ view <- function(x, title = NULL, ...) UseMethod("view")
 #' @export
 view.default <- function(x, title = NULL, ...) {
   if (interactive()) {
-    View(x)
+    utils::View(x)
   }
   invisible(x)
 }
