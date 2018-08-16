16e7e4cd1796071c1e6ed225893e3ae06131d53d
Kirill Müller <krlmlr+r@mailbox.org>
2018-08-16 16:55:18 +0200

    Define view() as  a plain non-generic function

--- R/view.R
+++ R/view.R
@@ -1,7 +1,6 @@
 #' View an object
 #'
-#' By default, calls [utils::View()] on the input and returns it,
-#' invisibly.
+#' Calls [utils::View()] on the input and returns it, invisibly.
 #' The RStudio IDE overrides `utils::View()`, this is picked up
 #' correctly.
 #'
@@ -12,41 +11,12 @@
 #'
 #' @export
 view <- function(x, title = NULL, ...) {
-  UseMethod("view")
-}
-
-#' @export
-view.default <- function(x, title = NULL, ...) {
   if (is.null(title)) {
-    title <- enlabel_s3(x)
+    title <- expr_deparse(enexpr(x))
   }
 
   view_fun <- get("View", envir = as.environment("package:utils"))
   view_fun(x, title)
 
   invisible(x)
-}
-
-# Dirty hack, remove after r-lib/rlang#303 is implemented
-enlabel_s3 <- function(x) {
-  method_call <- sys.call(-1)
-  method_name <- gsub("[.].*$", "", as.character(method_call[[1]]))
-
-  generic_call <- find_generic_call(method_name)
-  if (!is.null(generic_call)) {
-    x_arg <- match.call(get(method_name), generic_call)[["x"]]
-  } else {
-    x_arg <- enexpr(x)
-  }
-
-  expr_deparse(x_arg)
-}
-
-find_generic_call <- function(method_name) {
-  calls <- sys.calls()
-  for (call in rev(calls)) {
-    if (as.character(call[[1]]) == method_name) return(call)
-  }
-
-  NULL
 }
