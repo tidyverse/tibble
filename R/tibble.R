48d9d5d8fe814122ac5e3c2a02e6ae07fcc4f9a0
Kirill Müller <krlmlr+r@mailbox.org>
2018-10-06 18:29:19 +0200

    Add examples for hierarchical columns

--- R/tibble.R
+++ R/tibble.R
@@ -38,52 +38,79 @@
 #'   [name-repair] for more detail on name repair.
 #' @export
 #' @examples
+#' # Unnamed arguments are named with their expression:
 #' a <- 1:5
-#' tibble(a, b = a * 2)
+#' tibble(a, a * 2)
+#'
+#' # Scalars (vectors of length one) are recycled:
 #' tibble(a, b = a * 2, c = 1)
+#'
+#' # Columns are available in subsequent expressions:
 #' tibble(x = runif(10), y = x * 2)
 #'
-#' lst(n = 5, x = runif(n))
-#'
-#' # tibble never coerces its inputs
+#' # tibble() never coerces its inputs,
 #' str(tibble(letters))
 #' str(tibble(x = list(diag(1), diag(2))))
 #'
-#' # or munges column names (unless requested)
+#' # or munges column names (unless requested),
 #' tibble(`a + b` = 1:5)
 #'
-#' # but it forces you to take charge of names, if they need repair
-#' \dontrun{
-#' tibble(x = 1, x = 2)
-#' }
+#' # but it forces you to take charge of names, if they need repair:
+#' try(tibble(x = 1, x = 2))
 #' tibble(x = 1, x = 2, .name_repair = "unique")
 #' tibble(x = 1, x = 2, .name_repair = "minimal")
 #'
-#' ## by default, non-syntactic names are allowed
+#' ## By default, non-syntactic names are allowed,
 #' df <- tibble(`a 1` = 1, `a 2` = 2)
-#' ## because you can still index by name
+#' ## because you can still index by name:
 #' df[["a 1"]]
 #' df$`a 1`
+#' with(df, `a 1`)
 #'
-#' ## syntactic names are easier to work with, though, and you can request them
+#' ## Syntactic names are easier to work with, though, and you can request them:
 #' df <- tibble(`a 1` = 1, `a 2` = 2, .name_repair = "syntactic")
 #' df$a.1
 #'
-#' ## you can specify your own name repair function
+#' ## You can specify your own name repair function:
 #' tibble(x = 1, x = 2, .name_repair = make.unique)
 #'
 #' fix_names <- function(x) gsub("\\s+", "_", x)
 #' tibble(`year 1` = 1, `year 2` = 2, .name_repair = fix_names)
 #'
-#' # You can splice-unquote a list of quotes and formulas
+#' # Tibbles can contain columns that are tibbles or matrices
+#' # if the number of rows is consistent:
+#' tibble(
+#'   a = 1:3,
+#'   b = tibble(
+#'     c = 4:6,
+#'     d = 7:9
+#'   ),
+#'   e = tibble(
+#'     f = tibble(
+#'       g = letters[1:3]
+#'     )
+#'   )
+#' )
+#' tibble(
+#'   a = 1:4,
+#'   b = diag(4),
+#'   c = cov(iris[1:4])
+#' )
+#'
+#' # data can not contain POSIXlt columns, or tibbles or matrices
+#' # with inconsistent number of rows:
+#' try(tibble(y = strptime("2000/01/01", "%x")))
+#' try(tibble(a = 1:3, b = tibble(c = 4:7)))
+#'
+#' # You can splice-unquote a list of quosures and expressions:
 #' tibble(!!!list(x = rlang::quo(1:10), y = quote(x * 2)))
 #'
-#' # data frames can only contain 1d atomic vectors and lists
-#' # and can not contain POSIXlt
-#' \dontrun{
-#' tibble(x = tibble(1, 2, 3))
-#' tibble(y = strptime("2000/01/01", "%x"))
-#' }
+#' # lst() behaves very similarly, without restriction on the length
+#' # of the individual elements:
+#' lst(n = 5, x = runif(n))
+#'
+#' lst(!!!list(n = rlang::quo(2 + 3), y = quote(runif(n))))
+#'
 #' @aliases tbl_df-class
 tibble <- function(...,
                    .rows = NULL,
