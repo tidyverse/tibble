607e2d695e9afad0967ff3ef9440a8c7b135701b
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-10-10 18:59:39 -0700

    Port/adapt lots of tests from @krlmlr's branch

--- tests/testthat/test-syntactic_names.R
+++ tests/testthat/test-syntactic_names.R
@@ -75,3 +75,98 @@ test_that("syntactic_names() is idempotent", {
   x <- c(NA, "", "x", "x", "a1:", "_x_y}")
   expect_identical(syntactic_names(x), syntactic_names(syntactic_names(x)))
 })
+
+test_that("zero-length input", {
+  expect_equal(syntactic_names(character()), character())
+})
+
+test_that("syntactic names", {
+  expect_equal(syntactic_names(letters), letters)
+})
+
+test_that("dupes", {
+  expect_equal(
+    syntactic_names(c("a", "b", "a", "c", "b")),
+    c("a..1", "b..2", "a..3", "c", "b..5")
+  )
+})
+
+## TODO: figure this out
+test_that("solo empty or NA", {
+  expect_equal(syntactic_names(""), "...1")
+  expect_equal(syntactic_names(NA_character_), "...1")
+})
+
+test_that("dot", {
+  expect_equal(syntactic_names("."), ".")
+})
+
+test_that("dot, dot", {
+  expect_equal(syntactic_names(c(".", ".")), c("...1", "...2"))
+})
+
+## TODO: figure this out
+test_that("empty, dot", {
+  expect_equal(syntactic_names(c("", ".")), c("...1", "."))
+})
+
+## TODO: figure this out
+test_that("empty, empty, dot", {
+  expect_equal(syntactic_names(c("", "", ".")), c("...1", "...2", "."))
+})
+
+test_that("dot, empty, dot", {
+  expect_equal(syntactic_names(c(".", "", ".")), c("...1", "...2", "...3"))
+})
+
+## TODO: figure this out
+test_that("stripping '..j' gets interesting", {
+  expect_equal(syntactic_names("..1"), "...1")
+  expect_equal(syntactic_names("..13"), "...13")
+  expect_equal(syntactic_names("if..2"), ".if")
+})
+
+test_that("message", {
+  expect_message(
+    syntactic_names(c("", "")),
+    "New names:\n* `` -> ...1\n* `` -> ...2\n",
+    fixed = TRUE
+  )
+})
+
+test_that("quiet", {
+  expect_message(
+    syntactic_names("", quiet = TRUE),
+    NA
+  )
+})
+
+# set_syntactic_names ---------------------------------------------------------
+test_that("zero-length inputs given character names", {
+  out <- set_syntactic_names(character())
+  expect_equal(names(out), character())
+})
+
+test_that("unnamed input gives uniquely named output", {
+  out <- set_syntactic_names(1:3)
+  expect_equal(names(out), c("...1", "...2", "...3"))
+})
+
+test_that("messages by default", {
+  expect_message(
+    set_syntactic_names(set_names(1, "a:b")),
+    "New names:\n* `a:b` -> a.b\n",
+    fixed = TRUE
+  )
+})
+
+test_that("quiet = TRUE", {
+  expect_message(set_syntactic_names(set_names(1, ""), quiet = TRUE), NA)
+})
+
+test_that("non-syntactic names", {
+  out <- set_syntactic_names(set_names(1, "a b"))
+  expect_equal(names(out), "a.b")
+
+  expect_equal(syntactic_names("a b"), "a.b")
+})
