d86afb79ee9170d37df4609d21d8b9105812a38e
Kirill Müller <krlmlr+r@mailbox.org>
2018-10-13 15:12:16 +0200

    New tibble_row()

---
+++ R/row.R
@@ -0,0 +1,34 @@
+#' Construct a tibble row
+#'
+#' Constructs a tibble that is guaranteed to have only one row.
+#' Complex objects are always wrapped in a list.
+#'
+#' @inheritParams tibble
+#' @export
+#' @examples
+#' tibble_row(a = 1, b = 2:3, c = lm(hp ~ cyl, mtcars), d = list(1))
+tibble_row <- function(...,
+                       .name_repair = c("check_unique", "unique", "syntactic", "minimal")) {
+
+  xs <- quos(..., .named = TRUE)
+  xlq <- lst_quos(xs, transform = expand_wrap)
+  lst_to_tibble(xlq$output, .rows = 1L, .name_repair)
+}
+
+expand_wrap <- function(x, i) {
+  x[[i]] <- wrap_non_atomic(x[[i]])
+  x
+}
+
+#' @rdname tibble_row
+#' @usage NULL
+wrap_non_atomic <- function(x) {
+  #' @details
+  #' The function tests each column with [is_atomic()] and `n = 1`.
+  if (!is_atomic(x, 1L)) {
+    #' If the test fails, it is wrapped in a list.
+    list(x)
+  } else {
+    x
+  }
+}
