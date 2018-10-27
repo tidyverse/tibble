aedf20147a9b24c75d9e63cf53cfe93916c49e8b
Kirill Müller <krlmlr+r@mailbox.org>
2018-10-27 15:52:15 +0200

    Document subsetting

--- R/subsetting.R
+++ R/subsetting.R
@@ -1,3 +1,64 @@
+#' Subsetting tibbles
+#'
+#' @description
+#' Accessing columns, rows, or cells via `$`, `[[`, or `[` works very much like
+#' [with regular data frames][base::Extract.data.frame]. However, the
+#' behavior is different for tibbles and data frames in some cases:
+#' * `[` always returns a tibble by default, even if
+#'   only one column is accessed.  The `drop` argument is supported, but you
+#'   have to request it explicitly.
+#' * Partial matching of column names with `$` and `[[` is not supported, a
+#'   warning is given and `NULL` is returned.
+#'
+#' Unstable return type and implicit partial matching can lead to surprises and
+#' bugs that are hard to catch. If you rely on code that requires the original
+#' data frame behavior, coerce to a data frame via [as.data.frame()].
+#'
+#' @name subsetting
+#' @examples
+#' df <- data.frame(a = 1:3, bc = 4:6)
+#' tbl <- tibble(a = 1:3, bc = 4:6)
+#'
+#' # Same behavior:
+#' tbl[1, ]
+#' tbl[1, c("bc", "a")]
+#' tbl[, c("bc", "a")]
+#' tbl[c("bc", "a")]
+#' tbl["a"]
+#' tbl$a
+#' tbl[["a"]]
+#'
+#' # Different behavior:
+#' df[, "a"]
+#' tbl[, "a"]
+#' tbl[, "a", drop = TRUE]
+#' as.data.frame(tbl)[, "a"]
+#'
+#' df[1, , drop = TRUE]
+#' tbl[1, , drop = TRUE]
+#' as.list(tbl[1, ])
+#'
+#' df$b
+#' tbl$b
+#'
+#' df[["b", exact = FALSE]]
+#' tbl[["b", exact = FALSE]]
+#'
+#' df$bd <- c("n", "e", "w")
+#' tbl$bd <- c("n", "e", "w")
+#' df$b
+#' tbl$b
+#'
+#' df$b <- 7:9
+#' tbl$b <- 7:9
+#' df$b
+#' tbl$b
+NULL
+
+#' @rdname subsetting
+#' @param i,j Row and column indices. If `j` is omitted, `i` is used as column index.
+#' @param ... Ignored.
+#' @param exact Ignored, with a warning.
 #' @export
 `[[.tbl_df` <- function(x, i, j, ..., exact = TRUE) {
   if (!exact) {
@@ -10,16 +71,21 @@
   NextMethod()
 }
 
+#' @rdname subsetting
+#' @inheritParams base::`[.data.frame`
 #' @export
-`$.tbl_df` <- function(x, i) {
-  if (is.character(i)) {
-    ret <- .subset2(x, i)
-    if (is.null(ret)) warningc("Unknown or uninitialised column: '", i, "'.")
+`$.tbl_df` <- function(x, name) {
+  if (is.character(name)) {
+    ret <- .subset2(x, name)
+    if (is.null(ret)) warningc("Unknown or uninitialised column: '", name, "'.")
     return(ret)
   }
-  .subset2(x, i)
+  .subset2(x, name)
 }
 
+#' @rdname subsetting
+#' @param drop Coerce to a vector if fetching one column via `tbl[, j]` .
+#'   Default `FALSE`, ignored when accessing a column via `tbl[j]` .
 #' @export
 `[.tbl_df` <- function(x, i, j, drop = FALSE) {
   nr <- nrow(x)
