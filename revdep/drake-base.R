055efddee5810d2cd15bfba07e4f73182611640c
Kirill Müller <krlmlr+r@mailbox.org>
2018-11-05 10:10:10 +0000

    Tweaks: with bioconductur, all recursive revdeps

--- revdep/drake-base.R
+++ revdep/drake-base.R
@@ -2,7 +2,7 @@ library(drake)
 library(tidyverse)
 library(glue)
 
-options(repos = revdepcheck:::get_repos(FALSE))
+options(repos = revdepcheck:::get_repos(bioc = TRUE))
 
 get_this_pkg <- function() {
   desc::desc_get("Package") %>% unname()
@@ -30,7 +30,7 @@ get_plan_deps <- function() {
   plan_deps <- drake_plan(
     available = available.packages(),
     this_pkg = get_this_pkg(),
-    revdeps = tools::package_dependencies(this_pkg, available, 'most', reverse = TRUE) %>% flatten(),
+    revdeps = tools::package_dependencies(this_pkg, available, 'most', reverse = TRUE, recursive = TRUE) %>% flatten(),
     first_level_deps = tools::package_dependencies(revdeps, available, 'most'),
     all_level_deps = tools::package_dependencies(first_level_deps %>% flatten(), available, recursive = TRUE),
     base_pkgs = get_base_pkgs(),
