0382dee72a9c27ae3d8c238aaec9f4a91a3f08a7
Kirill Müller <krlmlr+r@mailbox.org>
2018-11-15 21:24:05 +0000

    Sorted analysis

--- revdep/drake-analyze.R
+++ revdep/drake-analyze.R
@@ -5,5 +5,6 @@ all <- drake::readd(compare_all)
 error <- map_chr(map(all, class), 1) == "try-error"
 
 succeeded <- all[!error]
+succeeded <- succeeded[sort(names(succeeded))]
 
 withr::with_output_sink("revdep/new-problems.md", revdepcheck::revdep_report_problems(results = succeeded))
