5b4dbe4a92a48343fa1c2f6d2a38f26e79d1273a
Kirill Müller <krlmlr+r@mailbox.org>
2018-12-19 23:47:10 +0100

    Patch test

--- tests/testthat/test-tbl-df.R
+++ tests/testthat/test-tbl-df.R
@@ -327,17 +327,17 @@ test_that("$ doesn't do partial matching", {
   expect_error(df$partial, NA)
 })
 
-# is.tibble ---------------------------------------------------------------
+# is_tibble ---------------------------------------------------------------
 
-test_that("is.tibble", {
-  expect_false(is.tibble(iris))
-  expect_true(is.tibble(as_tibble(iris)))
-  expect_false(is.tibble(NULL))
-  expect_false(is.tibble(0))
+test_that("is_tibble", {
+  expect_false(is_tibble(iris))
+  expect_true(is_tibble(as_tibble(iris)))
+  expect_false(is_tibble(NULL))
+  expect_false(is_tibble(0))
 })
 
 test_that("is_tibble", {
-  expect_identical(is.tibble, is_tibble)
+  expect_identical(is.tibble(iris), is_tibble(iris))
 })
 
 # new_tibble --------------------------------------------------------------
