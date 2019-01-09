597219c726cf273f4e7460c811d852e1eea6034e
Kirill Müller <krlmlr+r@mailbox.org>
2019-01-09 21:39:16 +0100

    Move docs to gh-pages

--- tic.R
+++ tic.R
@@ -14,5 +14,5 @@ if (Sys.getenv("BUILD_PKGDOWN") != "") {
 
   get_stage("deploy") %>%
     add_step(step_build_pkgdown()) %>%
-    add_step(step_push_deploy())
+    add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
 }
