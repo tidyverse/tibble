966bc64510810a71800d90dd84126bf84792df9e
Kirill Müller <krlmlr+r@mailbox.org>
2019-01-13 16:58:27 +0100

    R-devel also tests devel versions of packages

--- tic.R
+++ tic.R
@@ -1,5 +1,10 @@
 add_package_checks()
 
+if (Sys.getenv("DEV_VERSIONS") != "") {
+  get_stage("install") %>%
+    add_step(step_install_github(c("r-lib/rlang", "r-lib/cli", "r-lib/crayon", "r-lib/pillar", "r-lib/pkgconfig")))
+}
+
 if (Sys.getenv("BUILD_PKGDOWN") != "" && ci()$get_branch() == "master") {
   # pkgdown documentation can be built optionally. Other example criteria:
   # - `inherits(ci(), "TravisCI")`: Only for Travis CI
