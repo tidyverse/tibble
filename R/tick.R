04b2cda2a3e9a28c79efb8dc6419b87edf4a7a6d
Kirill Müller <krlmlr+r@mailbox.org>
2019-06-20 00:43:23 +0200

    Reimplement is_syntactic()

--- R/tick.R
+++ R/tick.R
@@ -3,18 +3,23 @@ tick <- function(x) {
   ifelse(is.na(x), "NA", encodeString(x, quote = "`"))
 }
 
-make_syntactic <- function(x) {
-  vctrs:::make_syntactic(x)
-}
-
-is_syntactic <- function(x) {
-  ret <- (make_syntactic(x) == x)
-  ret[is.na(x)] <- FALSE
-  ret
-}
-
 tick_if_needed <- function(x) {
   needs_ticks <- !is_syntactic(x)
   x[needs_ticks] <- tick(x[needs_ticks])
   x
+}
+
+is_syntactic <- function(x) {
+  ret <- rep_along(x, FALSE)
+  valid <- which(!is.na(x))
+  ret[valid] <- is_syntactic_impl(x[valid])
+  ret
+}
+
+is_syntactic_impl <- function(x) {
+  unchanged_after_repair <- (x == make.names(x))
+  # http://r.789695.n4.nabble.com/Dots-are-not-fixed-by-make-names-td4752920.html
+  dot_dot_dot_or_numbers <- grepl("^(?:(?:[.][.][.])|(?:[.][.][0-9]+))$", x)
+
+  unchanged_after_repair & !dot_dot_dot_or_numbers
 }
