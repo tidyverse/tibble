57b2e945335a272942c65af02ada89139f444ed6
Kirill Müller <krlmlr+r@mailbox.org>
2019-12-31 13:13:03 +0100

    Mark deprecated

--- R/as_tibble.R
+++ R/as_tibble.R
@@ -41,7 +41,10 @@
 #'     into this column and the `row.names` attribute is deleted.
 #'  Read more in [rownames].
 
-#' @param _n,validate For compatibility only, do not use for new code.
+#' @param _n,validate
+#'   \lifecycle{soft-deprecated}
+#'
+#'   For compatibility only, do not use for new code.
 #' @export
 #' @examples
 #' m <- matrix(rnorm(50), ncol = 5)
