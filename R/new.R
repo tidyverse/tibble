19484f9cb0b8eb44fe4dc16357d2e7cb9e1e20b8
Kirill Müller <krlmlr+r@mailbox.org>
2020-02-14 07:19:33 +0100

    Revert "Implement compat for sf"
    This reverts commit edf6fb1220d6911014cb03a5c2f2d01f059bd5d6.

--- R/new.R
+++ R/new.R
@@ -114,13 +114,7 @@ validate_nrow <- function(names, lengths, nrow) {
 }
 
 update_tibble_attrs <- function(x, ...) {
-  # Compat for https://github.com/r-spatial/sf/pull/1232
-  attribs <- list(...)
-  if (has_length(attribs) && is_named(attribs)) {
-    attributes(x)[names(attribs)] <- attribs
-  }
-
-  x
+  structure(x, ...)
 }
 
 tibble_class <- c("tbl_df", "tbl", "data.frame")
