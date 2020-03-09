334005929fea233559e8bd89a50649cf6246130b
Kirill Müller <krlmlr+r@mailbox.org>
2020-03-09 13:25:18 +0100

    Use verify_output()

--- tests/testthat/helper-expectations.R
+++ tests/testthat/helper-expectations.R
@@ -24,29 +24,3 @@ expect_error_cnd <- function(object, class, message = NULL, ..., .fixed = TRUE) 
     expect_equal(cnd[names(exp_fields)], exp_fields)
   }
 }
-
-expect_error_relax <- function(object, ...) {
-  expect_error(object)
-}
-
-expect_known_tibble_error_output <- function(...) {
-  quos <- enquos(...)
-
-  vals <- map(quos, eval_tidy)
-
-  functions <- map_chr(pluck(map(map(quos, quo_get_expr), as.list), 1), as_name)
-
-  skip_on_non_utf8_locale()
-  skip_on_cran()
-
-  headers <- paste0("\n## ", functions, "()\n\n")
-  header_same <- c(FALSE, headers[-1] == headers[-length(headers)])
-  headers[header_same] <- ""
-
-  output <- paste0(
-    "# Errors\n",
-    paste0(headers, map_chr(vals, cnd_message), collapse = "\n\n")
-  )
-
-  expect_known_output(cat(output), "errors.txt")
-}
