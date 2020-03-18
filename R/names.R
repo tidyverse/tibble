18c992485931c2e28b12350ae97b8b6c388dc7ec
Kirill Müller <krlmlr+r@mailbox.org>
2020-03-18 08:01:07 +0100

    Adapt to dev vctrs

--- R/names.R
+++ R/names.R
@@ -34,19 +34,31 @@ subclass_name_repair_errors <- function(expr, name, details = NULL) {
   tryCatch(
     force(expr),
 
+    # FIXME: use cnd$names with vctrs >= 0.3.0
     vctrs_error_names_cannot_be_empty = function(cnd) {
-      cnd <- error_column_must_be_named(cnd$locations, parent = cnd)
+      cnd <- error_column_must_be_named(detect_empty_names(name), parent = cnd)
       cnd$body <- details
 
       cnd_signal(cnd)
     },
     vctrs_error_names_cannot_be_dot_dot = function(cnd) {
-      cnd <- error_column_must_not_be_dot_dot(cnd$locations, parent = cnd)
+      cnd <- error_column_must_not_be_dot_dot(detect_dot_dot(name), parent = cnd)
       cnd_signal(cnd)
     },
     vctrs_error_names_must_be_unique = function(cnd) {
-      cnd <- error_column_names_must_be_unique(name[cnd$locations], parent = cnd)
+      cnd <- error_column_names_must_be_unique(detect_duplicates(name), parent = cnd)
       cnd_signal(cnd)
     }
   )
+}
+
+# Anticipate vctrs 0.3.0 release: locations replaced by names
+detect_empty_names <- function(names) {
+  which(names == "")
+}
+detect_dot_dot <- function(names) {
+  grep("^[.][.](?:[.]|[1-9][0-9]*)$", names)
+}
+detect_duplicates <- function(names) {
+  names[which(duplicated(names))]
 }
