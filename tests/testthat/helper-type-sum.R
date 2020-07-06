fca52d98c16994d12ef8515db8ee9e20b6155c41
Kirill Müller <krlmlr+r@mailbox.org>
2020-07-06 05:04:04 +0200

    Use s3_register()

--- tests/testthat/helper-type-sum.R
+++ tests/testthat/helper-type-sum.R
@@ -6,13 +6,13 @@ type_sum.override_type_sum <- function(x, ...) {
   "SC"
 }
 
-registerS3method("type_sum", "override_type_sum", type_sum.override_type_sum, envir = asNamespace("pillar"))
+vctrs::s3_register("pillar::type_sum", "override_type_sum")
 
 `[.override_type_sum` <- function(x, ...) {
   as_override_type_sum(NextMethod())
 }
 
-registerS3method("[", "override_type_sum", `[.override_type_sum`, envir = asNamespace("tibble"))
+vctrs::s3_register("base::[", "override_type_sum")
 
 as_override_tbl_sum <- function(x) {
   structure(x, class = c("override_tbl_sum", class(x)))
@@ -22,4 +22,4 @@ tbl_sum.override_tbl_sum <- function(x, ...) {
   c(NextMethod(), "Overridden" = "tbl_sum")
 }
 
-registerS3method("tbl_sum", "override_tbl_sum", tbl_sum.override_tbl_sum, envir = asNamespace("tibble"))
+vctrs::s3_register("tibble::tbl_sum", "override_tbl_sum")
