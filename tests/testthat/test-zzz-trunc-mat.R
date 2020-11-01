673e2a226484b9f534dd61484a202030a4abc993
Kirill Müller <krlmlr+r@mailbox.org>
2020-11-01 11:33:36 +0100

    Qualify

--- tests/testthat/test-zzz-trunc-mat.R
+++ tests/testthat/test-zzz-trunc-mat.R
@@ -1,7 +1,7 @@
 scoped_lifecycle_silence()
 
 test_that("interface of print() identical to trunc_mat()", {
-  if (packageVersion("pillar") >= "1.4.99") {
+  if (utils::packageVersion("pillar") >= "1.4.99") {
     print_arg_names <- names(formals(pillar:::print.tbl))
   } else {
     print_arg_names <- names(formals(print.tbl))
