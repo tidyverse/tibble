703522adf43ed6695be9b74a1e819e899aca6d2b
Kirill Müller <krlmlr+r@mailbox.org>
2021-07-30 17:35:39 +0200

    Legacy

--- R/options-tibble.R
+++ R/options-tibble.R
@@ -1,11 +1,9 @@
-## user-facing docs kept in `formatting` topic; see utils-format.R
-## Exception: tibble.view_max, in `tibble-package`
+## Legacy, for trunc_mat() and friends
 op.tibble <- list(
-  tibble.print_max = 20L,
   tibble.print_min = 10L,
+  tibble.print_max = 20L,
   tibble.width = NULL,
-  tibble.max_extra_cols = 100L,
-  tibble.view_max = 1000L
+  tibble.max_extra_cols = 100L
 )
 
 tibble_opt <- function(x, dplyr = TRUE) {
