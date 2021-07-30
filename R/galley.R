63b58197849930eecdaca33b30aedcee1ccd1e2e
Kirill Müller <krlmlr+r@mailbox.org>
2021-07-30 05:41:41 +0200

    Error diagnosis

--- R/galley.R
+++ R/galley.R
@@ -11,16 +11,37 @@ vignette_path <- function(name) {
   abort(paste0("Can't find vignette `", name, "`."))
 }
 
+local_methods <- function(..., .frame = rlang::caller_env()) {
+  rlang::local_bindings(..., .env = rlang::global_env(), .frame = .frame)
+}
+
 render_galley <- function(name) {
   path <- tempfile(fileext = ".md")
 
-  suppressMessages(capture.output(
+  out <- evaluate::evaluate(quote(
     rmarkdown::render(
       vignette_path(name),
       output_file = path,
       output_format = rmarkdown::md_document(preserve_yaml = TRUE)
     )
   ))
+
+  if (!file.exists(path)) {
+    my_replay <- function(x) UseMethod("my_replay")
+
+    my_replay_message <- function(x) evaluate::replay(conditionMessage(x))
+
+    local_methods(
+      my_replay.default = function(x) evaluate::replay(x),
+      my_replay.message = my_replay_message,
+      my_replay.warning = my_replay_message,
+      my_replay.error = my_replay_message
+    )
+    writeLines("")
+    lapply(out, my_replay)
+    writeLines("")
+    rlang::abort(paste0("Error rendering ", name))
+  }
 
   path
 }
