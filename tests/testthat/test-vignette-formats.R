b6420008dab6e5e4e9ca0ec37f6c9ca4459a17d7
Kirill Müller <krlmlr+r@mailbox.org>
2021-07-30 16:18:54 +0200

    Skip

--- tests/testthat/test-vignette-formats.R
+++ tests/testthat/test-vignette-formats.R
@@ -1,3 +1,4 @@
 test_that("formats vignette", {
+  skip_if_not_installed("pillar", "1.6.2")
   test_galley("formats")
 })
--- tests/testthat/test-vignette-types.R
+++ tests/testthat/test-vignette-types.R
@@ -1,3 +1,4 @@
 test_that("types vignette", {
+  skip_if_not_installed("pillar", "1.6.2")
   test_galley("types")
 })
