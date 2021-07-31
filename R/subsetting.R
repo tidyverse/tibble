b3ab924ae8ee4f5a7b97a4ecaab06bba030a53a9
Kirill Müller <krlmlr+r@mailbox.org>
2021-07-31 17:19:37 +0200

    Adapt to rlang 0.4.11

--- R/subsetting.R
+++ R/subsetting.R
@@ -168,7 +168,8 @@ NULL
 
   # Side effect: check scalar
   if (is.symbol(j)) {
-    j <- as_utf8_character(j)
+    # FIXME: as_utf8_character() needs rlang > 0.4.11
+    j <- chr_unserialise_unicode(as.character(j))
   } else {
     if (!is.vector(j) || length(j) != 1L || is.na(j) || (is.numeric(j) && j < 0) || is.logical(j)) {
       vectbl_as_col_location2(j, length(x), j_arg = j_arg, assign = TRUE)
