7aa54e67d6ceb31c81172c7d18d28ea9ce088888
krlmlr <krlmlr@users.noreply.github.com>
2022-01-18 01:47:57 +0000

    Auto-update from GitHub Actions
    Run: https://github.com/tidyverse/tibble/actions/runs/1710598047

--- tests/testthat/test-tribble.R
+++ tests/testthat/test-tribble.R
@@ -217,7 +217,7 @@ test_that("frame_matrix cannot have list columns", {
 test_that("tribble and frame_matrix cannot have named arguments", {
   expect_tibble_error(
     extract_frame_data_from_dots(
-      ~x,   ~y,
+      ~x, ~y,
       "a" = 1:3,
       "b" = 4:6
     ),
