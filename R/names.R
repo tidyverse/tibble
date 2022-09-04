a093b56bded37a64d86a555594a76f7c68ae2be7
Kirill Müller <krlmlr+r@mailbox.org>
2022-09-04 19:08:01 +0200

    abort_column_names_cannot_be_...

--- R/names.R
+++ R/names.R
@@ -25,15 +25,15 @@ repaired_names <- function(name,
 
 # Errors ------------------------------------------------------------------
 
-abort_column_names_cannot_be_empty <- function(names, repair_hint, details = NULL, parent = NULL, call = my_caller_call()) {
+abort_column_names_cannot_be_empty <- function(names, repair_hint, details = NULL, parent = NULL, call = my_caller_env()) {
   tibble_abort(invalid_df("must be named", names, use_repair(repair_hint)), names = names, parent = parent, call = call)
 }
 
-abort_column_names_cannot_be_dot_dot <- function(names, repair_hint, parent = NULL, call = my_caller_call()) {
+abort_column_names_cannot_be_dot_dot <- function(names, repair_hint, parent = NULL, call = my_caller_env()) {
   tibble_abort(invalid_df("must not have names of the form ... or ..j", names, use_repair(repair_hint)), names = names, parent = parent, call = call)
 }
 
-abort_column_names_must_be_unique <- function(names, repair_hint, parent = NULL, call = my_caller_call()) {
+abort_column_names_must_be_unique <- function(names, repair_hint, parent = NULL, call = my_caller_env()) {
   tibble_abort(invalid_df("must not be duplicated", names, use_repair(repair_hint), message = "Column name(s)"), names = names, parent = parent, call = call)
 }
 
