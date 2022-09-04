28980ba878ea9274230dc77c9b8659d412cd877c
Kirill Müller <krlmlr+r@mailbox.org>
2022-09-04 10:31:01 +0200

    Remove dead code

--- tests/testthat/helper-expectations.R
+++ tests/testthat/helper-expectations.R
@@ -14,17 +14,6 @@ expect_cnd_equivalent <- function(actual, expected) {
   expect_equal(actual, expected)
 }
 
-expect_error_cnd <- function(object, class, message = NULL, ..., .fixed = TRUE) {
-  cnd <- expect_error(object, regexp = message, class = class, fixed = .fixed)
-  expect_true(inherits_all(cnd, class))
-
-  exp_fields <- list2(...)
-  if (has_length(exp_fields)) {
-    expect_true(is_empty(setdiff(!!names(exp_fields), names(cnd))))
-    expect_equal(cnd[names(exp_fields)], exp_fields)
-  }
-}
-
 expect_snapshot_with_error <- function(code) {
   code <- rlang::enexpr(code)
 
