655fc25c80ac7a0e4b1e035f4ad988c6091375aa
Kirill Müller <krlmlr+r@mailbox.org>
2023-03-04 13:14:52 +0100

    chore: Fix r-oldrel-4 checks

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,6 +1,14 @@
-local_options(
-  warnPartialMatchArgs = TRUE,
-  warnPartialMatchAttr = TRUE,
-  warnPartialMatchDollar = TRUE,
-  .frame = testthat::teardown_env()
-)
+if (getRversion() >= "3.6") {
+  local_options(
+    warnPartialMatchArgs = TRUE,
+    warnPartialMatchAttr = TRUE,
+    warnPartialMatchDollar = TRUE,
+    .frame = testthat::teardown_env()
+  )
+} else {
+  local_options(
+    warnPartialMatchAttr = TRUE,
+    warnPartialMatchDollar = TRUE,
+    .frame = testthat::teardown_env()
+  )
+}
