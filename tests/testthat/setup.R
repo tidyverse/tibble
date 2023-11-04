5606fc165759b5a09ccb553dd2780be91d93ea53
Kirill Müller <kirill@cynkra.com>
2023-11-04 09:36:34 +0100

    test: Override :: to avoid failures in tests without suggested packages

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,3 +1,16 @@
+colon_colon <- `::`
+
+`::` <- function(x, y) {
+  x_sym <- ensym(x)
+  y_sym <- ensym(y)
+  tryCatch(
+    inject(colon_colon(!!x_sym, !!y_sym)),
+    packageNotFoundError = function(e) {
+      skip_if_not_installed(as_string(x_sym))
+    }
+  )
+}
+
 if (getRversion() >= "3.6") {
   local_options(
     warnPartialMatchArgs = TRUE,
