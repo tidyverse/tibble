2e5707d90f0a390bb840381b829ae6e1c17f3a1d
Kirill Müller <kirill@cynkra.com>
2023-11-10 10:10:53 +0100

    test: Skip tests if packages are missing

--- tests/testthat/test-vignette-types.R
+++ tests/testthat/test-vignette-types.R
@@ -1,3 +1,7 @@
 test_that("types vignette", {
+  skip_if_not_installed("dplyr")
+  skip_if_not_installed("tidyr")
+  skip_if_not_installed("purrr")
+
   test_galley("types", variant = rlang_variant())
 })
