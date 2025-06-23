4327066eb32bdc9ccbeedcff303e640c7c25f7c7
Michael Chirico <chiricom@google.com>
2025-06-23 15:59:44 -0700

    also in setup, and also simplify(?) branching

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -11,17 +11,12 @@ colon_colon <- `::`
   )
 }
 
+
+rlang::local_options(
+  warnPartialMatchAttr = TRUE,
+  warnPartialMatchDollar = TRUE,
+  .frame = testthat::teardown_env()
+)
 if (getRversion() >= "3.6") {
-  local_options(
-    warnPartialMatchArgs = TRUE,
-    warnPartialMatchAttr = TRUE,
-    warnPartialMatchDollar = TRUE,
-    .frame = testthat::teardown_env()
-  )
-} else {
-  local_options(
-    warnPartialMatchAttr = TRUE,
-    warnPartialMatchDollar = TRUE,
-    .frame = testthat::teardown_env()
-  )
+  rlang::local_options(warnPartialMatchArgs = TRUE)
 }
