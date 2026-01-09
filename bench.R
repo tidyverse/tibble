ba9dd92a1af2509689927c01399b45724ea336ed
Kirill Müller <kirill@cynkra.com>
2026-01-09 11:43:38 +0000

    URLs

---
+++ bench.R
@@ -0,0 +1,23 @@
+cpp11::cpp_source(code = '
+#include <cpp11/R.hpp>
+#include <Rinternals.h>
+
+[[cpp11::register]]
+int row_names_simple(SEXP x) {
+  SEXP row_names = Rf_getAttrib(x, R_RowNamesSymbol);
+  R_xlen_t nrow = Rf_xlength(row_names);
+  return (int)nrow;
+}
+
+[[cpp11::register]]
+int row_names_new(SEXP x) {
+  return (int)R_nrow(x);
+}'
+)
+
+data <- data.frame(a = 1:20)
+bench::mark(
+  row_names_simple(data),
+  row_names_new(data),
+  max_iterations = 1e7
+)
